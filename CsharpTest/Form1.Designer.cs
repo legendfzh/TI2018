c5f7562cbe9b296916d4591a45e5bd80d7e5d902
Tagore <183552658@qq.com>
2018-06-03 23:49:17 +0800

    添加项目文件。

---
+++ CsharpTest/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace CsharpTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort3 = new System.IO.Ports.SerialPort(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "扫描设备";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "连接1小车";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "断开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 24;
+            this.ListBox.Location = new System.Drawing.Point(208, 32);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(278, 244);
+            this.ListBox.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(505, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(465, 463);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(505, 568);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(282, 35);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(813, 556);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 54);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "发送";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(269, 480);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(217, 54);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "全局开始";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(269, 410);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(217, 54);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "全局暂停";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(34, 422);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 35);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "xxyy";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(34, 480);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(217, 54);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "宝物信息";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(94, 288);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(157, 54);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "选手1暂停";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(34, 150);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(157, 54);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "连接2小车";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(269, 288);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(157, 54);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "选手2暂停";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 571);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(394, 24);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "上位机由电机系学生科协技术部开发";
+            // 
+            // serialPort3
+            // 
+            this.serialPort3.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort3_DataReceived);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(637, 513);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(150, 28);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "发给小车1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(820, 513);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(150, 28);
+            this.checkBox2.TabIndex = 20;
+            this.checkBox2.Text = "发给小车2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(269, 348);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(157, 54);
+            this.button11.TabIndex = 22;
+            this.button11.Text = "选手2继续";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(94, 348);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(157, 54);
+            this.button12.TabIndex = 21;
+            this.button12.Text = "选手1继续";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 636);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "上位机（决赛版）";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ CsharpTest/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace CsharpTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Form.CheckForIllegalCrossThreadCalls = false;
+            string[] ArryPort = SerialPort.GetPortNames();
+            for (int i = 0; i < ArryPort.Length; i++)
+            {
+                ListBox.Items.Add(ArryPort[i]);
+            }
+
+        }
+        private void sendMessage(string Msg,bool to1,bool to2)
+        {
+            try
+            {
+                if (to1)
+                {
+                    serialPort1.WriteLine(Msg);
+                    textBox1.AppendText(string.Concat("向小车1发送：", Msg, "\n"));
+                }
+                if (to2)
+                {
+                    serialPort3.WriteLine(Msg);
+                    textBox1.AppendText(string.Concat("向小车2发送：", Msg, "\n"));
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("发送错误！");
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Clear();
+            string[] ArryPort = SerialPort.GetPortNames();
+            for (int i = 0; i < ArryPort.Length; i++)
+            {
+                ListBox.Items.Add(ArryPort[i]);
+            }
+            //string[] ArryInfo = GetHarewareInfo(HardwareEnum.Win32_PnPEntity, "Name");
+
+
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string str = serialPort1.ReadExisting();
+            textBox1.AppendText(string.Concat("收到1号小车信息：",str, "\n","上位机并不想处理该信息╭(╯^╰)╮\n"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)//连接1小车
+        {
+            try
+            {
+                serialPort1.PortName = ListBox.SelectedItem.ToString();
+                serialPort1.Open();
+                textBox1.AppendText(string.Concat(ListBox.SelectedItem.ToString(),"选手1小车已连接！\n"));
+            }
+            catch
+            {
+                MessageBox.Show("连接错误！");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            serialPort3.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sendMessage(textBox2.Text,checkBox1.Checked,checkBox2.Checked);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            sendMessage(string.Concat("*TR", textBox3.Text, "*"),true,true);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sendMessage("*RU*",true,true);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            sendMessage("*ST*",true,true);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                sendMessage(textBox2.Text, checkBox1.Checked, checkBox2.Checked);
+                textBox2.Clear();
+            }
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)//选手1暂停
+        {
+            textBox1.AppendText(string.Concat("选手1申请暂停！\n"));
+            sendMessage("*ST*", true, false);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort3.PortName = ListBox.SelectedItem.ToString();
+                serialPort3.Open();
+                textBox1.AppendText(string.Concat(ListBox.SelectedItem.ToString(), "选手2小车已连接！\n"));
+            }
+            catch
+            {
+                MessageBox.Show("连接错误！");
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)//选手2暂停
+        {
+            textBox1.AppendText(string.Concat("选手2申请暂停！\n"));
+            sendMessage("*ST*", false, true);
+        }
+
+        private void serialPort3_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string str = serialPort1.ReadExisting();
+            textBox1.AppendText(string.Concat("收到2号小车信息：", str, "\n", "上位机并不想处理该信息╭(╯^╰)╮\n"));
+        }
+        
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(string.Concat("选手1暂停结束！\n"));
+            sendMessage("*RU*", true, false);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(string.Concat("选手2暂停结束！\n"));
+            sendMessage("*RU*", false, true);
+        }
+
+
+        private static string[] GetHarewareInfo(HardwareEnum hardType, string propKey)
+        {
+            List<string> strs = new List<string>();
+            try
+            {
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from " + hardType))
+                {
+                    var hardInfos = searcher.Get();
+                    foreach (var hardInfo in hardInfos)
+                    {
+                        if (hardInfo.Properties[propKey].Value != null)
+                        {
+                            String str = hardInfo.Properties[propKey].Value.ToString();
+                            strs.Add(str);
+                        }
+
+                    }
+                }
+                return strs.ToArray();
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                strs = null;
+            }
+        }//end of func GetHarewareInfo().
+        
+        public enum HardwareEnum
+        {
+            // 硬件
+            Win32_Processor, // CPU 处理器
+            Win32_PhysicalMemory, // 物理内存条
+            Win32_Keyboard, // 键盘
+            Win32_PointingDevice, // 点输入设备，包括鼠标。
+            Win32_FloppyDrive, // 软盘驱动器
+            Win32_DiskDrive, // 硬盘驱动器
+            Win32_CDROMDrive, // 光盘驱动器
+            Win32_BaseBoard, // 主板
+            Win32_BIOS, // BIOS 芯片
+            Win32_ParallelPort, // 并口
+            Win32_SerialPort, // 串口
+            Win32_SerialPortConfiguration, // 串口配置
+            Win32_SoundDevice, // 多媒体设置，一般指声卡。
+            Win32_SystemSlot, // 主板插槽 (ISA & PCI & AGP)
+            Win32_USBController, // USB 控制器
+            Win32_NetworkAdapter, // 网络适配器
+            Win32_NetworkAdapterConfiguration, // 网络适配器设置
+            Win32_Printer, // 打印机
+            Win32_PrinterConfiguration, // 打印机设置
+            Win32_PrintJob, // 打印机任务
+            Win32_TCPIPPrinterPort, // 打印机端口
+            Win32_POTSModem, // MODEM
+            Win32_POTSModemToSerialPort, // MODEM 端口
+            Win32_DesktopMonitor, // 显示器
+            Win32_DisplayConfiguration, // 显卡
+            Win32_DisplayControllerConfiguration, // 显卡设置
+            Win32_VideoController, // 显卡细节。
+            Win32_VideoSettings, // 显卡支持的显示模式。
+
+            // 操作系统
+            Win32_TimeZone, // 时区
+            Win32_SystemDriver, // 驱动程序
+            Win32_DiskPartition, // 磁盘分区
+            Win32_LogicalDisk, // 逻辑磁盘
+            Win32_LogicalDiskToPartition, // 逻辑磁盘所在分区及始末位置。
+            Win32_LogicalMemoryConfiguration, // 逻辑内存配置
+            Win32_PageFile, // 系统页文件信息
+            Win32_PageFileSetting, // 页文件设置
+            Win32_BootConfiguration, // 系统启动配置
+            Win32_ComputerSystem, // 计算机信息简要
+            Win32_OperatingSystem, // 操作系统信息
+            Win32_StartupCommand, // 系统自动启动程序
+            Win32_Service, // 系统安装的服务
+            Win32_Group, // 系统管理组
+            Win32_GroupUser, // 系统组帐号
+            Win32_UserAccount, // 用户帐号
+            Win32_Process, // 系统进程
+            Win32_Thread, // 系统线程
+            Win32_Share, // 共享
+            Win32_NetworkClient, // 已安装的网络客户端
+            Win32_NetworkProtocol, // 已安装的网络协议
+            Win32_PnPEntity,//all device
+        }
+    }
+}
+
+
